066e124187eb79499e53ae6f391ce66f7bc6731e
Joseph Kinahan <kinahanj25@gmail.com>
2023-12-06 22:24:44 +0000

    first commit w/ basic api working

---
+++ src/main/java/com/majortomdev/codegangs/CodegangsApplication.java
@@ -0,0 +1,12 @@
+package com.majortomdev.codegangs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodegangsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodegangsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/majortomdev/codegangs/CodegangsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.majortomdev.codegangs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodegangsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
